5949d18ea421a98b43828efee2df83d72dc4f7cc
podarblr2 <g.mathankumar@podar.org>
2026-01-29 12:19:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,12 +4,13 @@ import os
 
 app = Flask(__name__)
 
-@app.route("/health")
-def health():
-    return {"status": "ok"}
+@app.route("/")
+def home():
+    return {"message": "Backend is live!"}
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))  # <- THIS is important
     app.run(host="0.0.0.0", port=port)
+
 
 
